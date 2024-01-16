d0f8e22886c5b5be7c21eab59de0422c55396c37
Min-SungJo <123or@naver.com>
2024-01-16 15:41:51 +0900

    #1 - 사용자 로그인, 회원가입, 조회 로직 및 h2 설정

---
+++ src/main/java/com/pj/oil/controller/MemberController.java
@@ -0,0 +1,92 @@
+package com.pj.oil.controller;
+
+import com.pj.oil.dto.LoginRequestDto;
+import com.pj.oil.entity.Member;
+import com.pj.oil.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping(value = "/v1/member")
+    public ResponseEntity<List<Member>> findAllMembers() {
+        return ResponseEntity.ok(memberService.findAllMembers());
+    }
+
+    @GetMapping(value = "/v1/member/{id}")
+    public ResponseEntity<EntityModel<Member>> findById(@PathVariable Long id) {
+        Optional<Member> findMembers = memberService.findById(id);
+        if (findMembers.isEmpty()) { // 예외처리 구문
+//            throw new UserNotFoundException("id:" + id);
+        }
+        // 조회한 member 뿐만 아니라 모든 member 를 조회할 수 있는 link 를 entityModel 에 담기
+        EntityModel<Member> entityModel = EntityModel.of(findMembers.get());
+        WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).findAllMembers());
+        entityModel.add(link.withRel("all-members"));
+
+        return ResponseEntity.ok(entityModel);
+    }
+
+    @PostMapping(value = "/v1/member")
+    public ResponseEntity<EntityModel<Member>> signup(@RequestBody Member member) {
+        Long savedMemberId = memberService.signup(member);
+
+        Optional<Member> savedMember = memberService.findById(savedMemberId);
+        if (savedMember.isEmpty()) { // 예외처리 구문
+//            throw new UserNotFoundException("id:" + savedMemberId);
+        }
+
+        // 생성한 member 를 조회할 수 있는 uri 를 Location 헤더에 담기
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedMemberId)
+                .toUri();
+
+        // 생성한 member 뿐만 아니라 모든 member 를 조회할 수 있는 link 를 entityModel 에 담기
+        EntityModel<Member> entityModel = EntityModel.of(savedMember.get());
+        WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).findAllMembers());
+        entityModel.add(link.withRel("all-members"));
+
+        return ResponseEntity.created(location).body(entityModel);
+    }
+
+    @PostMapping(value = "/v1/login")
+    public ResponseEntity<EntityModel<Member>> login(@RequestBody LoginRequestDto loginRequestDto) {
+        Long loginMemberId = memberService.login(loginRequestDto);
+
+        Optional<Member> loginMember = memberService.findById(loginMemberId);
+        if (loginMember.isEmpty()) { // 예외처리 구문
+//            throw new UserNotFoundException("id:" + loginMemberId);
+        }
+
+        // 생성한 member 를 조회할 수 있는 uri 를 Location 헤더에 담기
+        // login 경로로 시작했기 때문에 path 를 새롭게 설정함
+        URI location = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/v1/member/{id}")
+                .buildAndExpand(loginMemberId)
+                .toUri();
+
+        // 생성한 member 뿐만 아니라 모든 member 를 조회할 수 있는 link 를 entityModel 에 담기
+        EntityModel<Member> entityModel = EntityModel.of(loginMember.get());
+        WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).findAllMembers());
+        entityModel.add(link.withRel("all-members"));
+
+        return ResponseEntity.created(location).body(entityModel);
+    }
+
+}
---
+++ src/main/java/com/pj/oil/service/MemberService.java
@@ -0,0 +1,73 @@
+package com.pj.oil.service;
+
+import com.pj.oil.dto.LoginRequestDto;
+import com.pj.oil.entity.Member;
+import com.pj.oil.repository.MemberRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final Logger LOGGER = LoggerFactory.getLogger(MemberService.class);
+
+    public List<Member> findAllMembers() {
+        List<Member> members = memberRepository.findAll();
+        LOGGER.info("[findAllMembers] response member size: {}", members.size());
+        return members;
+    }
+
+    public Long signup(Member member) {
+        LOGGER.info("[signup] request member: {}", member.toString());
+        validateDuplicateMember(member); // 중복 회원 검증
+        Member signupMember = memberRepository.save(member);
+        LOGGER.info("[signup] response member: {}", signupMember.toString());
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        LOGGER.info("[validateDuplicateMember] request member: {}", member.toString());
+        Optional<Member> findMembers = memberRepository.findMemberByUserId(member.getUserId());
+        if (findMembers.isPresent()) {
+            LOGGER.info("[validateDuplicateMember] member data dose existed : {}", member.toString());
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        } else {
+            LOGGER.info("[validateDuplicateMember] member data dose not existed");
+        }
+    }
+
+    public Optional<Member> findById(Long id) {
+        Optional<Member> findMembers = memberRepository.findById(id);
+        if (findMembers.isPresent()) {
+            LOGGER.info("[validateDuplicateMember] member data dose existed : {}", findMembers.toString());
+        }
+        LOGGER.info("[validateDuplicateMember] member data dose not existed, id: {}", id);
+        return findMembers;
+    }
+
+    public Long login(LoginRequestDto dto) {
+        LOGGER.info("[login] login data: {}", dto.toString());
+        String userId = dto.getUserId();
+        if(memberRepository.findMemberByUserId(userId).isEmpty()) {
+            LOGGER.info("[login] userId data dose not existed");
+            //예외처리 구문
+        }
+        String password = dto.getPassword();
+        if(memberRepository.findMemberByUserIdAndAndPassword(userId, password).isEmpty()){
+            LOGGER.info("[login] password data dose not matched");
+            // 예외처리 구문
+        }
+        Long loginMemberId = memberRepository.findMemberByUserIdAndAndPassword(userId, password).get().getId();
+        LOGGER.info("[login] member data dose exited, login member id: {}", loginMemberId);
+        return loginMemberId;
+    }
+}
