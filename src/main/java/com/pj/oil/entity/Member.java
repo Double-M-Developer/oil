4ac76604c32fcd51df7e858a085888dccc858c7a
Min-SungJo <123or@naver.com>
2024-01-16 20:48:14 +0900

    #1 - Swagger로 문서화 및 예외처리 구문 추가

--- src/main/java/com/pj/oil/entity/Member.java
+++ src/main/java/com/pj/oil/entity/Member.java
@@ -1,12 +1,14 @@
 package com.pj.oil.entity;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.DynamicUpdate;
 
 import java.time.LocalDate;
 
+@Schema(description = "db에 저장된 사용자 정보")
 @Getter
 @Entity
 @ToString
@@ -14,16 +16,25 @@ import java.time.LocalDate;
 @DynamicUpdate
 public class Member {
 
+    @Schema(description = "사용자 id")
     @Id @GeneratedValue
     private Long id;
+    @Schema(description = "사용자 로그인 id")
     private String userId;
+    @Schema(description = "사용자 로그인 pw")
     private String password;
+    @Schema(description = "사용자 이름")
     private String username;
+    @Schema(description = "사용자 닉네임")
     private String nickname;
+    @Schema(description = "사용자 이메일")
     private String email;
+    @Schema(description = "사용자 권한")
     @Enumerated(EnumType.STRING)
     private Role role;
+    @Schema(description = "사용자 가입일")
     private LocalDate issueDate;
+    @Schema(description = "사용자 계정 상태 - 일반, 삭제, 휴면")
     @Enumerated(EnumType.STRING)
     private UserStatus userStatus;
 
