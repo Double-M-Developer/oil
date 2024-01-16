3f3acad0867513290c54f66c67ef90c83f187335
Min-SungJo <123or@naver.com>
2024-01-16 15:40:40 +0900

    #1 - swagger, hateoas 의존성 추가 및 사용자 Bean 생성

---
+++ src/main/java/com/pj/oil/dto/LoginRequestDto.java
@@ -0,0 +1,18 @@
+package com.pj.oil.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter @ToString
+public class LoginRequestDto {
+
+    private String userId;
+    private String password;
+
+    @Builder
+    public LoginRequestDto(String userId, String password) {
+        this.userId = userId;
+        this.password = password;
+    }
+}
