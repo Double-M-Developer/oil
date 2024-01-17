12397ba4af6af63ffc9760ac59ffb0a5dc9c5bbe
Min-SungJo <123or@naver.com>
2024-01-17 16:54:04 +0900

    #4 - 주유소 테이블 정규화,테스트 재작성, 더미 데이터 수정

--- src/test/java/com/pj/oil/service/GasStationServiceTest.java
+++ src/test/java/com/pj/oil/service/GasStationServiceTest.java
@@ -1,5 +1,6 @@
 package com.pj.oil.service;
 
+import com.pj.oil.dto.GasStationPriceDto;
 import com.pj.oil.entity.GasStation;
 import com.pj.oil.repository.GasStationRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,6 +21,22 @@ import static org.junit.jupiter.api.Assertions.*;
 @Transactional
 class GasStationServiceTest {
 
+    public static void printGasStationList(List<GasStation> list) {
+        for (GasStation o : list) {
+            System.out.println(o.toString());
+        }
+    }
+
+    public static void printGasStationPriceDtoList(List<GasStationPriceDto> list) {
+        for (GasStationPriceDto o : list) {
+            System.out.println(o.toString());
+        }
+    }
+
+    public static void printGasStation(GasStation object) {
+        System.out.println(object.toString());
+    }
+
     @Autowired
     GasStationService gasStationService;
     @Autowired
@@ -31,55 +48,63 @@ class GasStationServiceTest {
     }
 
     @Test
-    void 주유소_전체_조회() {
+    void 주유소_전체_및_가격조회() {
         List<GasStation> gasStations = gasStationService.findAllGasStations();
+        printGasStationList(gasStations);
         assertFalse(gasStations.isEmpty());
     }
 
     @Test
-    void 주유소_코드로_주유소_조회() {
+    void 주유소_코드에_해당하는_주유소_조회() {
         Optional<GasStation> gasStations = gasStationService.findByUniId("UNI_ID_1");
         //then
+        printGasStation(gasStations.get());
         gasStations.ifPresent(value -> assertEquals("UNI_ID_1", value.getUniId()));
     }
 
     @Test
-    void 주유소_제품으로_주유소_조회() {
-        List<GasStation> gasStations = gasStationService.findByProdcd("B027");
+    void 주유소_제품에_해당하는_주유소_가격_조회() {
+        List<GasStationPriceDto> gasStations = gasStationService.findByProdcd("B027");
+        printGasStationPriceDtoList(gasStations);
         assertFalse(gasStations.isEmpty());
     }
 
     @Test
-    void 시구군으로_주유소_조회() {
+    void 시구군에_해당하는_주유소_전체_조회() {
         List<GasStation> gasStations = gasStationService.findByArea("서울");
+        printGasStationList(gasStations);
         assertFalse(gasStations.isEmpty());
     }
 
     @Test
-    void 주유소_좌표로_주유소_조회() {
+    void 주유소_좌표에_해당하는_주유소_조회() {
         Optional<GasStation> gasStations = gasStationService.findByGisCoor(37.4979, 127.0276);
         //then
+        printGasStation(gasStations.get());
         gasStations.ifPresent(value -> assertEquals("UNI_ID_1", value.getUniId()));
     }
 
     @Test
-    void 주유소_상표로_주유소_조회() {
+    void 주유소_상표에_해당하는_주유소_조회() {
         List<GasStation> gasStations = gasStationService.findByPollDivCd("SKE");
         //then
+        printGasStationList(gasStations);
         assertFalse(gasStations.isEmpty());
     }
 
     @Test
-    void 주유소_상호로_주유소_조회() {
+    void 주유소_상호에_해당하는_주유소_조회() {
         Optional<GasStation> gasStations = gasStationService.findByOsNm("자영알뜰");
         //then
+        printGasStation(gasStations.get());
         gasStations.ifPresent(value -> assertEquals("UNI_ID_5", value.getUniId()));
     }
 
     @Test
-    void 업데이트_시간으로_주유소_검색() {
-        List<GasStation> gasStations = gasStationService.findByUpdatedAt("2024-01-17");
-        //then
+    void 가장_최근의_주유소_가격_정보_전체_조회() {
+        List<GasStationPriceDto> gasStations = gasStationService.findLatestPrices();
+        printGasStationPriceDtoList(gasStations);
         assertFalse(gasStations.isEmpty());
     }
+
 }
