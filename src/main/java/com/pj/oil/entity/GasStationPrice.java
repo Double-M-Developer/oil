02e38a954ee511013e83467717d686866680929f
Min-SungJo <123or@naver.com>
2024-01-18 15:26:49 +0900

    #4 - 주유소 및 지역의 가격  조회 로직

--- src/main/java/com/pj/oil/entity/GasStationPrice.java
+++ src/main/java/com/pj/oil/entity/GasStationPrice.java
@@ -20,15 +20,18 @@ public class GasStationPrice {
     @GeneratedValue
     @Schema(description = "주유소 가격 id")
     private Long id;
+
     @Schema(description = "주유소")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="uni_id")
     private GasStation gasStation;
+
     @Schema(description = "주유소 제품 - 구분(휘발유:B027, 경유:D047, 고급휘발유: B034, 실내등유:C004, 자동차부탄: K015)")
     @Enumerated(EnumType.STRING)
     private ProductCode prodcd;
     @Schema(description = "판매가격")
     private Integer price;
+
     @Schema(description = "컬럼 정보가 갱신된 시간")
     private String updatedAt;
 
