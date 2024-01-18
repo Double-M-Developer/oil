dd95d0f20ec67ace38c36c211dad631a8899168a
MoonJaeSeung <wotmdskfk32@naver.com>
2024-01-18 13:58:18 +0900

    css 파일 생성

--- src/main/java/com/pj/oil/MainController.java
+++ src/main/java/com/pj/oil/MainController.java
@@ -1,13 +1,14 @@
 package com.pj.oil;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 
     @RequestMapping("/")
-    public void main() {
-
+    public String main() {
+        return "index";
     }
 }
