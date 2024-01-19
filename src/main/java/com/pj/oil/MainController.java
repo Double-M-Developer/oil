347929ab5a7dd2ae55524e327ecffd6ebf40550f
MoonJaeSeung <wotmdskfk32@naver.com>
2024-01-19 20:19:35 +0900

    프론트엔드 UI 수정

--- src/main/java/com/pj/oil/MainController.java
+++ src/main/java/com/pj/oil/MainController.java
@@ -1,6 +1,7 @@
 package com.pj.oil;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,11 @@ public class MainController {
 
     @RequestMapping("/")
     public String main() {
+        return "index";
+    }
+
+    @GetMapping("/home")
+    public String home(){
         return "home";
     }
 
