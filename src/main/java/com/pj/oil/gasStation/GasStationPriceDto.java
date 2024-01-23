55f02b4da2b6196aac8fc0529fd26bd51434f1e6
Min-SungJo <123or@naver.com>
2024-01-23 15:04:22 +0900

    #7 프로젝트 엔터티 중심 구조로 변경

---
+++ src/main/java/com/pj/oil/gasStation/GasStationPriceDto.java
@@ -0,0 +1,40 @@
+package com.pj.oil.gasStation;
+
+import com.pj.oil.gasStation.Area;
+import com.pj.oil.gasStation.PollDivCode;
+import com.pj.oil.gasStation.ProductCode;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+@Getter
+@RequiredArgsConstructor
+@Setter
+@AllArgsConstructor
+@ToString
+public class GasStationPriceDto {
+    @Schema(description = "주유소 코드")
+    private String uniId;
+    @Schema(description = "주유소 지역 - 구분 미입력시 전국, 시도코드(2자리):해당시도 기준, 시군코드(4자리):해당시군 기준)")
+    private Area area;
+    @Schema(description = "상표(SKE:SK에너지, GSC:GS칼텍스, HDO:현대오일뱅크, SOL:S-OIL, RTE:자영알뜰, RTX:고속도로 알뜰, NHO:농협알뜰, ETC:자가상표, E1G: E1, SKG:SK가스 )")
+    private PollDivCode pollDivCd;
+    @Schema(description = "상호(중복X)")
+    private String osNm;
+    @Schema(description = "지번주소")
+    private String vanAdr;
+    @Schema(description = "도로명주소")
+    private String newAdr;
+    @Schema(description = "GIS X좌표(KATEC)")
+    private Double gisXCoor;
+    @Schema(description = "GIS Y좌표(KATEC)")
+    private Double gisYCoor;
+    @Schema(description = "주유소 가격 id")
+    private Long price_id;
+    @Schema(description = "주유소 제품 - 구분(휘발유:B027, 경유:D047, 고급휘발유: B034, 실내등유:C004, 자동차부탄: K015)")
+    private ProductCode prodcd;
+    @Schema(description = "판매가격")
+    private Integer price;
+    @Schema(description = "컬럼 정보가 갱신된 시간")
+    private String updatedAt;
+
+}
---
+++ src/main/java/com/pj/oil/member/MemberService.java
@@ -0,0 +1,90 @@
+package com.pj.oil.member;
+
+import com.pj.oil.auth.LoginRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final Logger LOGGER = LoggerFactory.getLogger(MemberService.class);
+
+    public List<Member> findAllMembers() {
+        List<Member> members = memberRepository.findAll();
+        LOGGER.info("[findAllMembers] response member size: {}", members.size());
+        return members;
+    }
+
+    @Transactional
+    public Long signup(Member member) {
+        LOGGER.info("[signup] request member: {}", member.toString());
+        validateDuplicateMember(member); // 중복 회원 검증
+        Member signupMember = memberRepository.save(member);
+        LOGGER.info("[signup] response member: {}", signupMember.toString());
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        LOGGER.info("[validateDuplicateMember] request member: {}", member.toString());
+        Optional<Member> findMembers = memberRepository.findMemberByUserId(member.getUserId());
+        if (findMembers.isPresent()) {
+            LOGGER.info("[validateDuplicateMember] member data dose existed : {}", member.toString());
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        } else {
+            LOGGER.info("[validateDuplicateMember] member data dose not existed");
+        }
+    }
+
+    public Optional<Member> findById(Long id) {
+        Optional<Member> findMembers = memberRepository.findById(id);
+        if (findMembers.isPresent()) {
+            LOGGER.info("[findById] member data dose existed : {}", findMembers.toString());
+        }
+        LOGGER.info("[findById] member data dose not existed, id: {}", id);
+        return findMembers;
+    }
+
+    public Long login(LoginRequestDto dto) {
+        LOGGER.info("[login] login data: {}", dto.toString());
+        String userId = dto.getUserId();
+        if(memberRepository.findMemberByUserId(userId).isEmpty()) {
+            LOGGER.info("[login] userId data dose not existed");
+            //예외처리 구문
+        }
+        String password = dto.getPassword();
+        if(memberRepository.findMemberByUserIdAndAndPassword(userId, password).isEmpty()){
+            LOGGER.info("[login] password data dose not matched");
+            // 예외처리 구문
+        }
+        Long loginMemberId = memberRepository.findMemberByUserIdAndAndPassword(userId, password).get().getId();
+        LOGGER.info("[login] member data dose exited, login member id: {}", loginMemberId);
+        return loginMemberId;
+    }
+
+    @Transactional
+    public Long updateMember(String userId, MemberUpdateFormDto dto) {
+        LOGGER.info("[updateMember] update data: {}", dto.toString());
+        Optional<Member> memberByUserId = memberRepository.findMemberByUserId(userId);
+        if(memberByUserId.isEmpty()) {
+            LOGGER.info("[updateMember] userId data dose not existed");
+            //예외처리 구문
+        }
+        LOGGER.info("[updateMember] member data dose existed, member: {}", memberByUserId);
+        Long updateRequestMemberId = memberByUserId.get().getId();
+        LOGGER.info("[updateMember] member data dose existed, member id: {}", updateRequestMemberId);
+
+        Member updateMember = memberByUserId.get();
+        updateMember.updateNickname(dto.getNickname());
+        updateMember.updatePassword(dto.getPassword());
+        return updateMember.getId();
+    }
+}
