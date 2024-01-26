19bf727c738dd2ed945bc88c8541c4b7a433045a
MoonJaeSeung <wotmdskfk32@naver.com>
2024-01-26 16:06:22 +0900

    test 수정

--- src/test/java/com/pj/oil/OilApplicationTests.java
+++ src/test/java/com/pj/oil/OilApplicationTests.java
@@ -1,14 +1,14 @@
-package com.pj.oil;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class OilApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.pj.oil;
 //
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class OilApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
+////
